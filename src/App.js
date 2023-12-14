63d6c9211f8bd2e80068a38ac329d2ecfa92db04
Pranav <91652265+pranavuppuluri@users.noreply.github.com>
2023-12-14 21:15:06 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Topbar from "./scenes/global/Topbar";
+import Sidebar from "./scenes/global/Sidebar";
+import Dashboard from "./scenes/dashboard";
+import Employees from "./scenes/employees";
+import Bar from "./scenes/bar";
+import CreateEmployee from "./scenes/form";
+import Pie from "./scenes/pie";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "./theme";
+import Calendar from "./scenes/calendar/calendar";
+import Projects from "./scenes/projects";
+import LeaveRequests from "./scenes/leave";
+
+function App() {
+  const [theme, colorMode] = useMode();
+  const [isSidebar, setIsSidebar] = useState(true);
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <Sidebar isSidebar={isSidebar} />
+          <main className="content">
+            <Topbar setIsSidebar={setIsSidebar} />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/employees" element={<Employees />} />
+              <Route path="/form" element={<CreateEmployee />} />
+              <Route path="/projects" element={<Projects />} />
+              <Route path="/leave" element={<LeaveRequests />} />
+              <Route path="/bar" element={<Bar />} />
+              <Route path="/pie" element={<Pie />} />
+              <Route path="/calendar" element={<Calendar />} />
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+}
+
+export default App;
